d8141da82ffab7bfb78e44691f38c8a72b1273b8
Andrei Ladonin <ladonin2001@outlook.com>
2020-04-16 10:14:58 +0300

    Добавьте файлы проекта.

---
+++ interfase/Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace interfase
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Student student1 = new Student() {Name= "vasya", Age= 20 };
+            Student student2 = new Student() { Name = "anna", Age = 18 };
+
+            Student[] students = new Student[] { student1, student2 };
+
+            Array.Sort(students);
+        }
+    }
+
+    public interface IShip
+    {
+        string Move(int distance);
+
+        string Fight();
+    }
+
+    public interface IDestroyble
+    {
+        string Destroy();
+    }
+
+    public struct Student : IComparable
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+
+        public int CompareTo(object obj)
+        {
+            Student student = (Student)obj;
+            if (this.Age > student.Age) return 1;
+            if (this.Age < student.Age) return -1;
+
+            return 0;
+        }
+    }
+
+
+    public class TransportShip : IShip, IDestroyble
+    {
+        public string Destroy()
+        {
+            return "КОРАБЛЬ КИЛЛ";
+        }
+
+        public string Fight()
+        {
+            return "Транспортный корабль не может вступить в бой";
+
+
+        }
+
+    public string Move(int distance)
+        {
+            double time = (double)distance / 1000;
+
+            return string.Format("Пройдено км {0} за время {1}", distance, time);
+
+        }
+    }
+}
---
+++ interfase/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов для изменения сведений,
+// связанные с этой сборкой.
+[assembly: AssemblyTitle("interfase")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("interfase")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// из модели COM задайте для атрибута ComVisible этого типа значение true.
+[assembly: ComVisible(false)]
+
+// Следующий GUID представляет идентификатор typelib, если этот проект доступен из модели COM
+[assembly: Guid("b154432e-b886-4611-8193-affbb02686b3")]
+
+// Сведения о версии сборки состоят из указанных ниже четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//      Номер сборки
+//      Номер редакции
+//
+// Можно задать все значения или принять номера сборки и редакции по умолчанию 
+// используя "*", как показано ниже:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
